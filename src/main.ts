3077b2b29c8ae58eaac3090ec32dd2009e55bc5b
Guada8a <jochoa10@ucol.mx>
2024-10-15 13:00:34 -0600

    feat(main): export wrapperApp

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,4 @@ import './index.css';
 export { Btn } from './Button/Button';
 export { Title } from './Title/Title';
 export { DashboardCard } from './Card/DashboardCard';
+export { WrapperApp } from './WrapperApp/WrapperApp';
