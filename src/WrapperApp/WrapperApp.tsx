e1426e07f0ff9164e49a9b302e745447e209edf8
Guada8a <jochoa10@ucol.mx>
2024-10-15 12:52:05 -0600

    feat(appconfig wrapper): update wrapper

--- src/WrapperApp/WrapperApp.tsx
+++ src/WrapperApp/WrapperApp.tsx
@@ -1,11 +1,9 @@
+// WrapperApp.tsx
 import React from 'react';
 import { ConfigProvider } from 'antd';
 import theme from '../config';
 import '../index.css';
-
-// Importaciones de locales de Ant Design
 import es_ES from 'antd/lib/locale/es_ES';
-
 
 interface WrapperAppProps {
   children: React.ReactNode;
