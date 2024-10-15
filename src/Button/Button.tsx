cba8fb8248dd9ef9143f0bb24d64ee1f143c419f
Guada8a <jochoa10@ucol.mx>
2024-10-15 14:51:32 -0600

    fix(button.tsx): deploy again

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,4 +1,7 @@
 import { Button } from "antd"
+import { ConfigProvider } from "antd"
+import es_ES from "antd/lib/locale/es_ES"
+import theme from "../config"
 
 interface ButtonProps {
   children: React.ReactNode
@@ -6,10 +9,15 @@ interface ButtonProps {
 
 export const Btn = ({ children }: ButtonProps) => {
   return (
+    <ConfigProvider
+      locale={es_ES}
+      componentSize='middle'
+      theme={theme}>
     <Button
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
     >
       {children}
     </Button>
+    </ConfigProvider>
   )
 }
