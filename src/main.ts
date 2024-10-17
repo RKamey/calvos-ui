28d7a732e415ca38504db093e36c7b3d1992a5fc
Branyoe <branyoe.dev@gmail.com>
2024-10-17 09:59:00 -0600

    feat: add generateAccessWrapper

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,4 @@ export { DashboardCard } from './Card/DashboardCard';
 export { WrapperApp } from './WrapperApp/WrapperApp';
 export { createAuthStore } from './useAuth/useAuth'
 export { SESSIONS_STATUS } from './useAuth/useAuth'
+export { generateAccessWrapper } from './AccessWrapper/generateAccessWrapper';
